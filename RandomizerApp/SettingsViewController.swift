25c98ec0d10c382404d0174c2cc154098cb4cf21
OSIIOSdev <OSIIOS.dev@gmail.com>
2025-03-27 11:38:38 -0700

    1. UI elements added

---
+++ RandomizerApp/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  RandomizerApp
+//
+//  Created by OSIIOSdev on 27.03.2025.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
